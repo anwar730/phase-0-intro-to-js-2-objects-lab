0580c4f02e1a807a5a95c9a35b0dff2319ce470a
Anwar Ismail <anwar.ismail@student.moringaschool.com>
2022-08-19 05:09:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+let employee={
+    name: 'Anwar',
+    streetAddress: '12 southC'
+
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const update={...employee}
+    update[key]=value
+    return update
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee.name="Sam"
+    employee.streetAddress='12 Broadway'
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee={...employee}
+    
+    delete newEmployee[key]
+    return newEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key,value){
+    employee[key] = value
+    return employee
+}
+
+
+   
